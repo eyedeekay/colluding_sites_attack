f73c4761cda245c5158f206eb9945547041b87fd
idk <problemsolver@openmailbox.org>
2018-03-01 23:29:16 -0500

    switching to a go echo service

--- http-headers.go
+++ http-headers.go
@@ -9,11 +9,13 @@ import (
 type blah struct{}
 
 func (b *blah) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    log.Println("the echo service is responding.")
 	for key, value := range r.Header {
 		fmt.Fprintf(w, "Temos o header: %s, com valor %s\n", key, value)
 	}
 }
 
 func main() {
+    log.Println("starting go echo service")
 	log.Fatal(http.ListenAndServe(":8080", &blah{}))
 }
