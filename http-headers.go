8bfa1bbb44f88704712de6fafc718decc792c83b
idk <problemsolver@openmailbox.org>
2018-03-12 23:05:46 -0400

    attempting to fix syntax crap

--- http-headers.go
+++ http-headers.go
@@ -20,17 +20,17 @@ func (b *blah) ServeHTTP(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "  <title> What is my Base64? </title>\n")
     fmt.Fprintf(w, "  <link rel=\"stylesheet\" href=\"http://4pvyyb3phqznc6e6fjewty2fpbb4p3ub2q27ojheitcg4nai6p5q.b32.i2p/css/styles.css\">\n")
     fmt.Fprintf(w, "</head>\n")
-
     fmt.Fprintf(w, "  <body>\n")
-    fmt.Fprintf(w, "  <p>Attempting to force resource retrieval over plain https</p>\n")
+    fmt.Fprintf(w, "  <p>\n")
+    fmt.Fprintf(w, "    Attempting to force resource retrieval over plain https")
+    fmt.Fprintf(w, "  </p>\n")
     fmt.Fprintf(w, "  <iframe src=\"https://api.ipify.org\" />\n")
     fmt.Fprintf(w, "  <script type=\"application/javascript\">\n")
     fmt.Fprintf(w, "    function getIP(json) {\n")
-    fmt.Fprintf(w, "      document.write(\"My public IP address is: \", json.ip);\n")
+    fmt.Fprintf(w, "      document.write(\"<pre><code>My public IP address is: </code></pre>\", json.ip);\n")
     fmt.Fprintf(w, "    }\n")
     fmt.Fprintf(w, "  </script>\n")
-
-    fmt.Fprintf(w, "  <script type=\"application/javascript\" src=\"https://api.ipify.org?format=jsonp&callback=getIP\"></script>")
+    fmt.Fprintf(w, "  <script type=\"application/javascript\" src=\"https://api.ipify.org\"></script>")
     fmt.Fprintf(w, "    <pre><code>\n")
 	for key, value := range r.Header {
         log.Println(key, value)
