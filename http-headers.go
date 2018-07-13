401c2556587fa39f32c3d296488cf1b220a767e1
idk <problemsolver@openmailbox.org>
2018-07-12 22:36:15 -0400

    getting ready for more extensive browser testing.

--- http-headers.go
+++ http-headers.go
@@ -9,45 +9,45 @@ import (
 type blah struct{}
 
 func (b *blah) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    log.Println("the echo service is responding.")
-    /*
-    fmt.Fprintf(w, "\n")
+	log.Println("the echo service is responding.")
+	/*
+	   fmt.Fprintf(w, "\n")
 
-    */
-    fmt.Fprintf(w, "<!DOCTYPE html>\n")
-    fmt.Fprintf(w, "<html>\n")
-    fmt.Fprintf(w, "<head>\n")
-    fmt.Fprintf(w, "  <title> What is my Base64? </title>\n")
-    fmt.Fprintf(w, "  <link rel=\"stylesheet\" href=\"http://4pvyyb3phqznc6e6fjewty2fpbb4p3ub2q27ojheitcg4nai6p5q.b32.i2p/css/styles.css\">\n")
-    fmt.Fprintf(w, "</head>\n")
-    fmt.Fprintf(w, "  <body>\n")
-    fmt.Fprintf(w, "  <p>\n")
-    fmt.Fprintf(w, "    Attempting to force resource retrieval over plain https")
-    fmt.Fprintf(w, "  </p>\n")
-    fmt.Fprintf(w, "    <pre><code>\n")
+	*/
+	fmt.Fprintf(w, "<!DOCTYPE html>\n")
+	fmt.Fprintf(w, "<html>\n")
+	fmt.Fprintf(w, "<head>\n")
+	fmt.Fprintf(w, "  <title> What is my Base64? </title>\n")
+	fmt.Fprintf(w, "  <link rel=\"stylesheet\" href=\"http://4pvyyb3phqznc6e6fjewty2fpbb4p3ub2q27ojheitcg4nai6p5q.b32.i2p/css/styles.css\">\n")
+	fmt.Fprintf(w, "</head>\n")
+	fmt.Fprintf(w, "  <body>\n")
+	fmt.Fprintf(w, "  <p>\n")
+	fmt.Fprintf(w, "    Attempting to force resource retrieval over plain https")
+	fmt.Fprintf(w, "  </p>\n")
+	fmt.Fprintf(w, "    <pre><code>\n")
 	for key, value := range r.Header {
-        log.Println(key, value)
+		log.Println(key, value)
 		fmt.Fprintf(w, "Header: %s, Value: %s\n", key, value)
 	}
-    fmt.Fprintf(w, "    </pre></code>\n")
-    fmt.Fprintf(w, "  <script type=\"application/javascript\">\n")
-    fmt.Fprintf(w, "    function getIP(json) {\n")
-    fmt.Fprintf(w, "      document.write(\"<pre><code>\");\n")
-    fmt.Fprintf(w, "      document.write(\"My public IP address is:\", json.ip);\n")
-    fmt.Fprintf(w, "      document.write(\"</pre></code>\");\n")
-    fmt.Fprintf(w, "    }\n")
-    fmt.Fprintf(w, "  </script>\n")
-    fmt.Fprintf(w, "  <script type=\"application/javascript\" src=\"https://api.ipify.org?format=jsonp&callback=getIP\"></script>\n")
-    fmt.Fprintf(w, "  </body>\n")
-    fmt.Fprintf(w, "</html>\n")
-    /*
+	fmt.Fprintf(w, "    </pre></code>\n")
+	fmt.Fprintf(w, "  <script type=\"application/javascript\">\n")
+	fmt.Fprintf(w, "    function getIP(json) {\n")
+	fmt.Fprintf(w, "      document.write(\"<pre><code>\");\n")
+	fmt.Fprintf(w, "      document.write(\"My public IP address is:\", json.ip);\n")
+	fmt.Fprintf(w, "      document.write(\"</pre></code>\");\n")
+	fmt.Fprintf(w, "    }\n")
+	fmt.Fprintf(w, "  </script>\n")
+	fmt.Fprintf(w, "  <script type=\"application/javascript\" src=\"https://api.ipify.org?format=jsonp&callback=getIP\"></script>\n")
+	fmt.Fprintf(w, "  </body>\n")
+	fmt.Fprintf(w, "</html>\n")
+	/*
 
-    fmt.Fprintf(w, "\n")
+	   fmt.Fprintf(w, "\n")
 
-    */
+	*/
 }
 
 func main() {
-    log.Println("starting go echo service")
+	log.Println("starting go echo service")
 	log.Fatal(http.ListenAndServe("0.0.0.0:8080", &blah{}))
 }
