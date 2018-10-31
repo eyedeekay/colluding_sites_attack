4bf48c5e1b25292bd8c14613e3202bb1e62b224c
idk <problemsolver@openmailbox.org>
2018-10-31 03:45:22 -0400

    use http server explicitly

--- http-headers.go
+++ http-headers.go
@@ -28,7 +28,7 @@ func randSeq(n int) string {
 
 func (b *blah) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	log.Println("the echo service is responding to a request on:", forwarder.Base32())
-	csp_header := fmt.Sprintf("default-src 'self' api.ipify.org %s;", *sourcesite)
+	csp_header := fmt.Sprintf("default-src 'self' api.ipify.org %s; ", *sourcesite)
 	csp_header += fmt.Sprintf("script-src 'self' api.ipify.org %s;", *sourcesite)
 	w.Header().Add("Content-Security-Policy", csp_header)
 	w.WriteHeader(http.StatusOK)
